a3c371f30569adb759d24a4d7e2c779ff023984a
Mikhail Okhapkin <mikhail.okhapkin@yandex.ru>
2023-04-29 23:49:58 +0300

    Make real names null in database
    Changed the user columns responsible for the first and last name. Now you don't have to fill them out when registering.

--- src/main/resources/db/migration/V1_0_0__Init.sql
+++ src/main/resources/db/migration/V1_0_0__Init.sql
@@ -3,8 +3,8 @@ CREATE TABLE IF NOT EXISTS user_t (
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    real_name VARCHAR(255) NOT NULL,
-    surname VARCHAR(255) NOT NULL,
+    real_name VARCHAR(255),
+    surname VARCHAR(255),
     active BOOLEAN NOT NULL
 );
 
